e318b867847acea0570a457d7d56848e2dddc6ba
trofinata1 <86907719+trofinata1@users.noreply.github.com>
2022-01-12 21:37:53 +0300

    Декоративные элементы главной страницы (#7)
    * Декоративные элементы и мелкие сетки главной страницы
    
    * Добавляет класс иконке логотипа на десктопе
    
    * Корректирует состояние фокуса у ссылок
    
    * Исправляет кнопку под ПП и исправляет бг с крышей
    
    * Поправляет картинку у фона
    
    Co-authored-by: Natalia <miss.happ2012@yandex.ru>

---
+++ source/js/script/script.js
@@ -0,0 +1,10 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--opened')) {
+    navMain.classList.remove('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--opened');
+  }
+});
